6cf2febc8b5c629fb6144964a62cb9a2de868b35
mauro <maurop123@gmail.com>
2017-11-07 22:16:26 -0500

    add layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <v-app>
-    <v-text-field
-      name="input-1"
-      label="Label Text"
-      id="testing"
-    ></v-text-field>
+    <v-container>
+      <v-layout>
+        <v-flex>
+          <v-text-field
+            name="input-1"
+            label="Label Text"
+            id="testing"
+            ></v-text-field>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-app>
 </template>
 
