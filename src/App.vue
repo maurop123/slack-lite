19922c7b3aea6fc4e74571d6782689c5aa334295
mauro <maurop123@gmail.com>
2017-11-08 10:52:11 -0500

    add firebase

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,17 @@
 </template>
 
 <script>
+  import firebase from 'firebase'
+
+  const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyD8Nl-dkVKGp3hyznikkh3jTRuOO2Bvh_k",
+    authDomain: "slack-lite.firebaseapp.com",
+    databaseURL: "https://slack-lite.firebaseio.com",
+  })
+
+  const db = firebaseApp.database()
+  const auth = firebase.auth()
+
   export default {
     data () {
       return {
