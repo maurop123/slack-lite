0f678bfd152267b85d98e30573a5f275b2f8eaae
mauro <maurop123@gmail.com>
2017-11-08 16:14:51 -0500

    changes for deploy

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -104,9 +104,9 @@ var webpackConfig = merge(baseWebpackConfig, {
     new SWPrecacheWebpackPlugin({
       cacheId: 'my-vue-app',
       filename: 'service-worker.js',
-      staticFileGlobs: ['dist/**/*.{js,html,css}'],
+      staticFileGlobs: ['docs/**/*.{js,html,css}'],
       minify: true,
-      stripPrefix: 'dist/'
+      stripPrefix: 'docs/'
     })
   ]
 })
