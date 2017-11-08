72131a7288ac8aa7cfa0a0234d0d8e2a4ad32cbe
mauro <maurop123@gmail.com>
2017-11-07 23:36:23 -0500

    fix layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-container>
-      <v-layout>
+      <v-layout column>
         <v-flex>
           <v-list>
             <template v-for="msg in messageList">
@@ -11,19 +11,21 @@
             </template>
           </v-list>
         </v-flex>
-      </v-layout>
-      <v-layout>
         <v-flex>
-          <v-text-field
-            label="Message"
-            v-model="message"
-            ></v-text-field>
-        </v-flex>
-        <v-flex xs1>
-          <v-btn flat icon color="blue"
-            v-on:click="sendMessage">
-            <v-icon>send</v-icon>
-          </v-btn>
+          <v-layout>
+            <v-flex>
+              <v-text-field
+                label="Message"
+                v-model="message"
+                ></v-text-field>
+            </v-flex>
+            <v-flex xs1>
+              <v-btn flat icon color="blue"
+                v-on:click="sendMessage">
+                <v-icon>send</v-icon>
+              </v-btn>
+            </v-flex>
+          </v-layout>
         </v-flex>
       </v-layout>
     </v-container>
