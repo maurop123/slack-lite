743b4294b7210c5e5833af91c60cc8434aa5680b
mauro <maurop123@gmail.com>
2017-11-08 14:29:33 -0500

    added toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <v-app>
+    <v-toolbar color="purple darken-4" dark>
+      <v-toolbar-title>
+        Slack Lite
+      </v-toolbar-title>
+    </v-toolbar>
     <router-view></router-view>
   </v-app>
 </template>
--- src/components/ChatPage.vue
+++ src/components/ChatPage.vue
@@ -1,12 +1,58 @@
 <template>
-  <h1>Hey Slacker!</h1>
+  <v-container fill-height>
+    <v-layout column>
+      <v-flex>
+        <v-list>
+          <template v-for="msg in messageList">
+            <v-list-tile>
+              {{ msg }}
+            </v-list-tile>
+            <v-divider></v-divider>
+          </template>
+        </v-list>
+      </v-flex>
+      <v-flex>
+        <v-layout>
+          <v-flex>
+            <v-text-field
+              label="Message"
+              v-model="message"
+              ></v-text-field>
+          </v-flex>
+          <v-flex xs1>
+            <v-btn flat icon color="blue"
+              v-on:click="sendMessage">
+              <v-icon>send</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
   export default {
     data () {
       return {
+        message: '',
+        messageList: []
+      }
+    },
+    methods: {
+      sendMessage: function() {
+        this.messageList.push(this.message)
+        this.message = ''
       }
     }
   }
 </script>
+
+<style>
+  .application--light .list {
+    background: none;
+  }
+  .container.fill-height {
+    align-items: flex-end;
+  }
+</style>
